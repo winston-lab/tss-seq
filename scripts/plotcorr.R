2118216850e1908e714a252ee4d5906941a28594
James Chuang <mrjc@bu.edu>
2017-11-05 10:49:10 -0500

    Got bored, updated correlation plotting with stat_hexbin+geom_point instead of geom_hexbin, added shading for pearson correlations.

--- scripts/plotcorr.R
+++ scripts/plotcorr.R
@@ -10,9 +10,8 @@ main = function(intable, pcount, samplelist, outpath){
     df = df %>% spread(sample, signal) %>% select(-name)
     
     maxsignal = max(df) + pcount
+    mincor = min(cor(df, use="complete.obs"))
     plots = list()
-    
-    placeholder = data.frame(x=c(0,1), y=c(0,1))
     
     #for each row
     for (i in 1:ncol(df)){
@@ -22,9 +21,12 @@ main = function(intable, pcount, samplelist, outpath){
             #upper right (correlation)
             if (i < j){
                 c = cor(df[,i], df[,j], use = "complete.obs")
-                plot = ggplot(data = placeholder, aes(x, y)) +
-                        geom_blank() +
-                        annotate("text", x=0.5, y=0.5, label=sprintf("%.2f",round(c,2)), size=10*c)
+                plot = ggplot(data = tibble(x=c(0,1), y=c(0,1), corr=c)) +
+                        geom_rect(aes(fill=corr), xmin=0, ymin=0, xmax=1, ymax=1) +
+                        annotate("text", x=0.5, y=0.5, label=sprintf("%.2f",round(c,2)), size=10*c) +
+                        scale_x_continuous(breaks=NULL) +
+                        scale_y_continuous(breaks=NULL) +
+                        scale_fill_distiller(palette="Blues", limits = c(mincor,1))
                 plots[[idx]] = plot
             }
             #top left to bot right diag (density)
@@ -47,7 +49,7 @@ main = function(intable, pcount, samplelist, outpath){
                             filter(!(xvalue < 6*pcount & yvalue < 6*pcount))
                 plot = ggplot(data = subdf, aes(x=xvalue+pcount, y=yvalue+pcount)) +
                             geom_abline(intercept = 0, slope=1, color="grey80", size=.5) +
-                            geom_hex(aes(fill=log10(..count..), color=log10(..count..)), binwidth=c(.04,.04), size=0) +
+                            stat_bin_hex(geom="point", aes(color=log10(..count..)), binwidth=c(.04,.04), size=.15, shape=16) +
                             scale_fill_viridis(option="inferno") +
                             scale_color_viridis(option="inferno") +
                             scale_x_log10(limit = c(pcount, maxsignal)) +
