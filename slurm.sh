be0c0d290366422545bf6f882c8b56114894543d
James Chuang <mrjc@bu.edu>
2017-12-15 10:58:37 -0500

    Update cluster execution to automatically recreate files where the code to generate them has changed.

--- slurm.sh
+++ slurm.sh
@@ -8,4 +8,4 @@
 #SBATCH -o snakemake.log
 #SBATCH -J TSSseq-snakemake
 
-snakemake -p --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --cluster "sbatch -p {cluster.queue} -n {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
+snakemake -p -R `snakemake --list-code-changes` --latency-wait 300 --rerun-incomplete --cluster-config cluster.yaml --use-conda --jobs 999 --cluster "sbatch -p {cluster.queue} -n {cluster.n} -t {cluster.time} --mem-per-cpu={cluster.mem} -J {cluster.name} -e {cluster.err} -o {cluster.log}"
